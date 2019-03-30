c7a56d2e9c992ac1e402a9520acf00854168b153
mu0gum <47498356+mu0gum@users.noreply.github.com>
2019-03-30 15:47:21 +0900

    Delete recommand_module.py

--- presentalk/recommand_module.py
+++
@@ -1,8 +0,0 @@
-class RecommandClass:
-
-    def __init__(self):
-        pass
-
-    def execute_command(self, mecab, message):
-        return_dict = {'command': 'recommand', 'message': []}
-        return return_dict
